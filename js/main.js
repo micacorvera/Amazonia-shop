191580237a75ae11d484c4e913c879333652a6d6
Micaela Corvera <micac.016@gmail.com>
2025-01-25 11:14:14 -0300

    agrego archivos al repositorio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//variable string
+let curso = "javascript"
+//variable numérica
+let comision = 7877
+//variable booleana
+let flex = true 
+
+//constantes
+const programadora = "micaela"
+
+//mostrar las variables o constantes en la consola
+console.log(programadora)
+console.log(curso)
+console.log(comision)
